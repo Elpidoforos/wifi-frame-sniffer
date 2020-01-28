71d52c97f2a066b8de6ff56df6e6de49f4257dac
Elpidoforos Arapantonis <elpidoforos.arapantonis@volvocars.com>
2020-01-28 06:49:55 +0100

    Remove comments

--- wpy_sniff.py
+++ wpy_sniff.py
@@ -8,20 +8,14 @@ import itertools
 args = sys.argv
 
 # Set the wireless interface in monitor mode
-
-
 def set_mon_mode(inf_name):
     mon_mode_wlan = subprocess.run(["airmon-ng", "start", inf_name])
 
 # Kill all running processes in regards to wireless com
-
-
 def kill_run_proc_wlan():
     kill_process = subprocess.run(["airmon-ng", "check", "kill"])
 
 # Change channel
-
-
 def channel_hop():
     print("Inside Channel Hop")
     channel_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48, 52, 56,
@@ -40,26 +34,20 @@ def channel_hop():
             time.sleep(5)
 
 # Capture beacon requests on different channels in the wireless interface
-
-
 def capture_beacons(inf_name_mon):
     capture_beacons = open("/tmp/capture_beacons.txt", "w+")
     while(1):
         capture_beacons = subprocess.call(
             ["/usr/sbin/tcpdump", "-i", inf_name_mon, "type", "mgt", "subtype", "beacon"], stdout=capture_beacons)
-        # print(capture_beacons)
 
 # Capture beacon requests on different channels in the wireless interface
-
-
 def capture_probes(inf_name_mon):
     capture_probes = open("/tmp/capture_probes.txt", "w+")
     while(1):
-        #capture_probes = subprocess.run(["/usr/sbin/tcpdump", "-i", inf_name_mon, "type", "mgt", "subtype", "probe-req"])
         capture_probes = subprocess.call(
             ["/usr/sbin/tcpdump", "-i", inf_name_mon, "type", "mgt", "subtype", "probe-req"], stdout=capture_probes)
 
-
+# The main function
 def main():
 
     kill_run_proc_wlan()
